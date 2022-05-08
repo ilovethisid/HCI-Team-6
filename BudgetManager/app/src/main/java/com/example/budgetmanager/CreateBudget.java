e91e15eeb4af01e2490e16167cae333dd130370e
Wiam Boumaazi <wiamboumaazi@Wiams-MBP.attlocal.net>
2022-05-08 11:56:57 -0700

    add profile and create budget pages

---
+++ BudgetManager/app/src/main/java/com/example/budgetmanager/CreateBudget.java
@@ -0,0 +1,102 @@
+package com.example.budgetmanager;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreateBudget#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreateBudget extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    AutoCompleteTextView txt;
+
+
+    public CreateBudget() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CreateBudget.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CreateBudget newInstance(String param1, String param2) {
+        CreateBudget fragment = new CreateBudget();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_create_budget, container, false);
+
+        String [] values =
+                {"Category","Food","Liquor","Supplies","Clothes","Books"};
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent1, View view1, int position1, long id1) {
+                if (parent1.getItemAtPosition(position1).equals("Category")){
+                }else {
+                    String item = parent1.getItemAtPosition(position1).toString();
+                    Toast.makeText(parent1.getContext(),"Selected: " +item, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        spinner.setAdapter(adapter);
+
+        return v;
+
+    }
+}
